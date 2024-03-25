274bd6c08ec41ad18bfa45f0041b10501349fa30
Alenteva <144173680+Alenteva@users.noreply.github.com>
2024-03-25 02:11:40 +0200

    .

--- src/components/Feedback/feedback.jsx
+++ src/components/Feedback/feedback.jsx
@@ -1,11 +1,13 @@
+import css from '../Feedback/feedback.module.css';
+
 const Feedback = ({ feedbackTypes, totalFeedback, totalPositive }) => {
   return (
     <ul>
-      <li>Good: {feedbackTypes.good}</li>
-      <li>Neutral: {feedbackTypes.neutral}</li>
-      <li> Bad: {feedbackTypes.bad}</li>
-      <li> Total: {totalFeedback}</li>
-      <li> Positive: {totalPositive}%</li>
+      <li className={css['feedback']}>Good: {feedbackTypes.good}</li>
+      <li className={css['feedback']}>Neutral: {feedbackTypes.neutral}</li>
+      <li className={css['feedback']}> Bad: {feedbackTypes.bad}</li>
+      <li className={css['feedback']}> Total: {totalFeedback}</li>
+      <li className={css['feedback']}> 😊 Positive: {totalPositive}%</li>
     </ul>
   );
 };
--- src/components/Options/options.jsx
+++ src/components/Options/options.jsx
@@ -5,18 +5,38 @@ const Options = ({ totalFeedback, updateFeedback, resetFeedbackType }) => {
     <div>
       <ul className={css['optionsBox']}>
         <li>
-          <button onClick={() => updateFeedback('good')}>Good 👍</button>
+          <button
+            className={css['optionsButton']}
+            onClick={() => updateFeedback('good')}
+          >
+            Good 👍
+          </button>
         </li>
         <li>
-          <button onClick={() => updateFeedback('neutral')}>Neutral 😐</button>
+          <button
+            className={css['optionsButton']}
+            onClick={() => updateFeedback('neutral')}
+          >
+            Neutral 😐
+          </button>
         </li>
         <li>
-          <button onClick={() => updateFeedback('bad')}>Bad 🙁</button>
+          <button
+            className={css['optionsButton']}
+            onClick={() => updateFeedback('bad')}
+          >
+            Bad 🙁
+          </button>
         </li>
 
         {totalFeedback > 0 && (
           <li>
-            <button onClick={resetFeedbackType}>Reset</button>
+            <button
+              className={css['optionsButton']}
+              onClick={resetFeedbackType}
+            >
+              Reset
+            </button>
           </li>
         )}
       </ul>
